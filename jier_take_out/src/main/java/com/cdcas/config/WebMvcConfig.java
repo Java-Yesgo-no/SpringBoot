c26997c34a785f343b8103f71b501385d113e3eb
赵志鑫 <zhaozhixin199@gmail.com>
2022-09-20 00:36:47 +0800

    "新增员工功能开发(分页,编辑，状态修改)"

--- jier_take_out/src/main/java/com/cdcas/config/WebMvcConfig.java
+++ jier_take_out/src/main/java/com/cdcas/config/WebMvcConfig.java
@@ -1,12 +1,14 @@
 package com.cdcas.config;
 
-import com.cdcas.controller.interceptor.MyInterceptor;
+import com.cdcas.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -23,7 +25,28 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
     }
 
-//    向servlet和controller中间添加拦截器
+
+    /**
+     * 扩展mvc框架的消息转换器
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("扩展消息转换器。。。");
+//        创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter=new MappingJackson2HttpMessageConverter();
+//        设置对象转换器，底层使用Jackson将Java对象转为json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+//        将上面的消息转换器对象追加到mvc框架的转换器集合中
+//        index为0是因为mvc自己有转换器，如果想使用自己的转换器
+//        需要要将其放在最前面
+        converters.add(0,messageConverter);
+
+
+        super.extendMessageConverters(converters);
+    }
+
+    //    向servlet和controller中间添加拦截器
 /*    @Autowired
     private MyInterceptor myInterceptor;
     @Override
--- jier_take_out/src/main/java/com/cdcas/controller/EmployeeController.java
+++ jier_take_out/src/main/java/com/cdcas/controller/EmployeeController.java
@@ -1,17 +1,15 @@
 package com.cdcas.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cdcas.common.R;
-import com.cdcas.mapper.EmployeeMapper;
 import com.cdcas.pojo.Employee;
 import com.cdcas.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
@@ -21,7 +19,7 @@ import java.time.LocalDateTime;
 @RequestMapping("/employee")
 public class EmployeeController {
     @Autowired
-    private EmployeeService employeeservice;
+    private EmployeeService employeeService;
 
     /**
      * 员工登录功能
@@ -46,7 +44,7 @@ public class EmployeeController {
 //        第二步:根据页面提交的用户名username查询数据库
         LambdaQueryWrapper<Employee> queryWrapper=new LambdaQueryWrapper<>();
         queryWrapper.eq(Employee::getUsername,employee.getUsername());
-        Employee emp=employeeservice.getOne(queryWrapper);
+        Employee emp=employeeService.getOne(queryWrapper);
 
 //        第三步:如果没有查询到则返回登录失败的结果queryWrapper = {LambdaQueryWrapper@7818}
         if (emp==null){
@@ -95,7 +93,75 @@ public class EmployeeController {
         employee.setUpdateUser(empManagerId);
 
 
-        employeeservice.save(employee);
+        employeeService.save(employee);
         return R.success("新增员工成功!");
+    }
+
+    /**
+     * 员工信息分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page<Employee>> page(int page,int pageSize,String name){
+        log.info("page={}  ,pageSize={},  name={}",page,pageSize,name);
+
+//        构造分页构造器
+        Page<Employee>  pageInfo=new Page<>(page,pageSize);
+
+//        构造条件构造器
+        LambdaQueryWrapper<Employee> queryWrapper=new LambdaQueryWrapper<>();
+
+//       条件1 添加过滤条件
+        queryWrapper.like(StringUtils.isNotEmpty(name),Employee::getUsername,name);
+
+//       条件2 添加排序条件
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+
+//        执行查询
+        employeeService.page(pageInfo,queryWrapper);
+
+        System.out.println(pageInfo);
+        return R.success(pageInfo);
+    }
+
+    /**
+     * 根据id修改员工的信息
+     * @param employee
+     * @return
+     */
+    @PutMapping
+    public R<String> update(HttpServletRequest request,@RequestBody Employee employee){
+        Employee employeeManager= (Employee) request.getSession().getAttribute("employee");
+
+        employee.setUpdateTime(LocalDateTime.now());
+
+        employee.setUpdateUser(employeeManager.getId());
+
+        /**
+         * 代码修复：
+         * 前端对Long类型的id处理导致从前端传过来的id有精度损失，
+         * 需要将id转化为字符串
+         * 具体实现步骤:
+         * 1.提供对象转换器JacksonObjectMapper,基于jacson进行
+         * Java对象到json数据的转换。
+         * 2.在WebMvcConfig配置类中扩展Springmvc的消息转换器，
+         * 在此消息转换器中使用提供的对象转换器进行Java对象到json
+         * 数据的转换
+         */
+        employeeService.updateById(employee);
+
+        return R.success("员工信息修改成功");
+    }
+        @GetMapping("/{id}")
+    public R<Employee> getById(@PathVariable Long id){
+        log.info("根据id查询员工信息...");
+        Employee employee=employeeService.getById(id);
+        if (employee!=null){
+            return R.success(employee);
+        }
+        return R.error("没有查到改员工");
     }
 }
