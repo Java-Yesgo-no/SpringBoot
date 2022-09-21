d24a0bb136e1bc4ab46976372c460c79c4c79d8c
赵志鑫 <zhaozhixin199@gmail.com>
2022-09-22 00:26:52 +0800

    "文件的下载"

--- jier_take_out/src/main/java/com/cdcas/controller/CommonController.java
+++ jier_take_out/src/main/java/com/cdcas/controller/CommonController.java
@@ -3,13 +3,16 @@ package com.cdcas.controller;
 import com.cdcas.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.time.LocalDateTime;
+import java.io.FileInputStream;
 import java.util.UUID;
 
 /**
@@ -25,17 +28,17 @@ public class CommonController {
 
     /**
      * 文件上传
+
      *
      * @param file
-     * @return
+                * @return
      */
-    @PostMapping("/upload")
-    public R<String> upload(MultipartFile file) throws Exception {
+        @PostMapping("/upload")
+        public R<String> upload(MultipartFile file) throws Exception {
 
 
 //        file是一个临时文件,需要转存到指定位置，否则本次请求完成后临时文件会删除
-        log.info(file.toString());
-
+            log.info(file.toString());
 //        原始文件名
 
         String originalFilename = file.getOriginalFilename();
@@ -48,13 +51,35 @@ public class CommonController {
         String fileName = UUID.randomUUID() + suffix;
 
 //        创建一个目录对象
-        File dir=new File(basePath);
+        File dir = new File(basePath);
 //        判断当前目录是否存在
-        if (!dir.exists()){
+        if (!dir.exists()) {
             dir.mkdir();
         }
 //        将临时文件存起来
         file.transferTo(new File(basePath + fileName));
         return R.success(fileName);
+    }
+
+    /**
+     * 文件下载
+     *
+     * @param name
+     */
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response) throws Exception {
+//        输入流，读取文件
+        FileInputStream fileInputStream = new FileInputStream(new File(basePath) + name);
+//        输出流，将文件写浏览器，在浏览器展示图片
+        ServletOutputStream outputStream = response.getOutputStream();
+//        设置返回类型
+        response.setContentType("image/jpeg");
+        int len=0;
+        byte[] bytes=new byte[1024];
+        while ((len=fileInputStream.read(bytes))!=-1){
+            outputStream.write(bytes,0,len);
+        }
+        fileInputStream.close();
+        outputStream.close();
     }
 }
